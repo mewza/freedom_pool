eb7c640c54c2735c1540cbfe42232b8aa0a4ed7b
mewza <subband@gmail.com>
2023-11-17 15:27:44 -1000

    Update freedom_pool.h
    Added 64-bit alignment for the internal allocated blocks

--- freedom_pool.h
+++ freedom_pool.h
@@ -1,28 +1,30 @@
-// freedom_pool.h
+// freedom_pool.h v1.1 ©2023 Dmitry Bodlyrev
 //
-// This is the most efficient memory allocator system you can find. I tried many: rpmalloc, tlsf, this one is IT
-// I tested it and it works great on iOS, OS X, I got about 30% speed up in doing multi-media rich app that uses
-// 3D OpenGL gfx, and audio
+// This is the most efficient block-pool memory management system you can find. I tried many before writing my own:
+// rpmalloc, tlsf, etc.
+//
+// I tested it live in a multimedia rich project I am working on that has OpenGL, and audio DSP, it works great no
+// weird crashes. Solid.
 //
 // This code is partially based off this block allocator concept:
 // https://www.codeproject.com/Articles/1180070/Simple-Variable-Size-Memory-Block-Allocator
-//
-// (C) 2023 Dmitry Bodlyrev
 
 #ifndef H_FREEDOM_POOL
 #define H_FREEDOM_POOL
 
 #include <stdlib.h>
+#include <assert.h>
 
 #ifdef __cplusplus
 #include <map>
 #include <iostream>
 #endif
 
+#include <dispatch/dispatch.h>
 #include <dispatch/queue.h>
+#include <assert.h>
+#include <malloc/malloc.h>
 #include "atomic_lock.h"
-
-#define MALLOC_V4SF_ALIGNMENT 64 // 64-byte alignment
 
 //#define DEBUG fprintf
 #define DEBUG_PRINTF
@@ -37,40 +39,40 @@ public:
     {
         internal = true;
         
-        m_FreeBlocksByOffset.empty();
-        m_FreeBlocksBySize.empty();
+        (void)m_FreeBlocksByOffset.empty();
+        (void)m_FreeBlocksBySize.empty();
         
         m_MaxSize = 0;
         m_FreeSize = m_MaxSize;
         m_Lock.init();
         
-        m_Data = (uint8_t*) ::malloc( m_MaxSize );
+        m_Data = (uint8_t*)_malloc64( m_MaxSize );
        // ExtendPool( 1024L ); // 1MB
     }
     ~FreedomPool()
     {
         if (m_Data)
-            ::free(m_Data);
+            _free64(m_Data);
         m_Data = NULL;
         m_Lock.exit();
     }
-    inline bool IsFull() const { return m_FreeSize == 0; };
-    inline bool IsEmpty() const { return m_FreeSize == m_MaxSize; };
-    inline int64_t GetMaxSize() const { return m_MaxSize; }
-    inline int64_t GetFreeSize() const { return m_FreeSize; }
-    inline int64_t GetUsedSize() const{ return m_MaxSize - m_FreeSize; }
+    __inline bool IsFull() const { return m_FreeSize == 0; };
+    __inline bool IsEmpty() const { return m_FreeSize == m_MaxSize; };
+    __inline int64_t GetMaxSize() const { return m_MaxSize; }
+    __inline int64_t GetFreeSize() const { return m_FreeSize; }
+    __inline int64_t GetUsedSize() const{ return m_MaxSize - m_FreeSize; }
 
-    inline int64_t GetNumFreeBlocks() const {
+    __inline int64_t GetNumFreeBlocks() const {
          return m_FreeBlocksByOffset.size();
      }
-    inline int64_t GetMaxFreeBlockSize() const {
+    __inline int64_t GetMaxFreeBlockSize() const {
          return !m_FreeBlocksBySize.empty() ? m_FreeBlocksBySize.rbegin()->first : 0;
      }
     
     void *calloc(size_t count, size_t size)
     {
         if (internal || !m_MaxSize)
-            return ::calloc(count, size);
+            return _malloc64(count * size);
         else
             return malloc(count * size);
     }
@@ -78,7 +80,7 @@ public:
     void *malloc(int64_t nb_bytes)
     {
         if (internal || !m_MaxSize) {
-            return ::malloc(nb_bytes);
+            return _malloc64(nb_bytes);
         }
        if (nb_bytes <= 0) {
            DEBUG_PRINTF(stderr, "FreedomPool: nb_bytes <= 0\n");
@@ -91,15 +93,6 @@ public:
             ExtendPool( GetMaxSize() - grow );
         }
         return malloc_internal(nb_bytes);
-       
-        /*
-        void *p, *p0 = malloc_internal(nb_bytes + MALLOC_V4SF_ALIGNMENT);
-        if (!p0) return (void *) 0;
-        ::memset(p0, 0, nb_bytes + MALLOC_V4SF_ALIGNMENT);
-        p = (void *) (((size_t) p0 + MALLOC_V4SF_ALIGNMENT) & (~((size_t) (MALLOC_V4SF_ALIGNMENT-1))));
-        *((void **) p - 1) = p0;
-         
-        return p;*/
     }
     
     void *realloc(void *p, int64_t new_size)
@@ -107,7 +100,7 @@ public:
         void *new_p;
         
         if (internal || !m_MaxSize)
-            return ::realloc(p, new_size);
+            return _realloc64(p, new_size);
         
         if (!p) return NULL;
         if (!(new_p = malloc(new_size)))
@@ -122,11 +115,11 @@ public:
     void free(void *p)
     {
         if (internal || !m_MaxSize) {
-            ::free(p);
+            _free64(p);
             return;
         }
-        if (p) free_internal(p);
-//        if (p) free_internal(*((void **) p - 1));
+    //    if (p) free_internal(p);
+     if (p) free_internal(*((void **) p - 1));
     }
     
      void ExtendPool(int64_t ExtraSize)
@@ -160,7 +153,7 @@ public:
          m_MaxSize += ExtraSize;
          m_FreeSize += ExtraSize;
 
-         m_Data = (uint8_t*)::realloc(m_Data, m_MaxSize);
+         m_Data = (uint8_t*)_realloc64(m_Data, m_MaxSize);
          assert(m_Data != NULL);
          
          
@@ -298,7 +291,37 @@ protected:
         m_Lock.unlock();
 
     }
-
+    void *_malloc64(int64_t nb_bytes)
+    {
+        void *p, *p0 = ::malloc(nb_bytes + 64);
+        if (!p0) return (void *) 0;
+        ::memset(p0, 0, nb_bytes + 64);
+        p = (void *) (((size_t) p0 + 64) & (~((size_t)63)));
+        *((void **) p - 1) = p0;
+        return p;
+    }
+    
+    void *_realloc64(void *p, int64_t nb_bytes)
+    {
+        if (!p) return NULL;
+        
+        void *pp = _malloc64(nb_bytes);
+        size_t sz = _msize64(p);
+        if (pp && sz > 0)
+        {
+            ::memcpy(pp, p, sz);
+            _free64(p);
+            return pp;
+        } else
+            return NULL;
+    }
+    size_t _msize64(void *p) {
+        return malloc_size(*((void **) p - 1));
+    }
+    void _free64(void *p) {
+        if (p) ::free(*((void **) p - 1));
+    }
+    
     
     uint8_t                *m_Data;
     int64_t                 m_MaxSize, m_FreeSize;
@@ -321,4 +344,4 @@ void operator delete[](void *p) throw();
 
 #endif // __cplusplus
 
-#endif // H_DYN_ALLOC
+#endif // H_FREEDOM_POOL
