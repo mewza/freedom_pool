5fcc305f8f021843b27ee7be9fc282afbbb3c163
mewza <subband@gmail.com>
2024-05-11 13:37:05 -1000

    Update freedom_pool.cpp
    Freedom Pool v1.21

--- freedom_pool.cpp
+++ freedom_pool.cpp
@@ -1,4 +1,4 @@
-// freedom_pool.cpp v1.2 (C)2023-2024 Dmitry Bodlyrev
+// freedom_pool.cpp v1.21 (C)2023-2024 Dmitry Bodlyrev
 //
 // This is the most efficient block-pool memory management system you can find. I tried many before writing my own:
 // rpmalloc, tlsf, many tlsf clones.
@@ -9,6 +9,8 @@
 // C++ wrappers for C++ new/new[]/delete/delete[]
 
 #include <malloc/malloc.h>
+#include <signal.h>
+
 #include "freedom_pool.h"
 
 real_malloc_ptr FreedomPool::real_malloc = NULL;
@@ -18,34 +20,58 @@ real_realloc_ptr FreedomPool::real_realloc = NULL;
 real_malloc_size_ptr FreedomPool::real_malloc_size = NULL;
 real_malloc_usable_size_ptr FreedomPool::real_malloc_usable_size = NULL;
 
-static uint64_t total_alloc = 0;
+static int64_t total_alloc = 0L;
+static int64_t total_max_alloc = 0L;
 
-#define Debugger() { assert(false); }
+// #define BREAK_ON_THRESH
 
+#define MBYTE               1048576L
+
+#define THRESH_DEBUG_BREAK  19L * MBYTE
+#define THRESH_DEBUG_PRINT  1L * MBYTE
+
+#define DEBUGGER_BREAK      raise(SIGINT);
+
+void reset_freedom_counters() {
+    total_alloc = 0;
+    total_max_alloc = 0;
+}
 #if !defined(DISABLE_MALLOC_FREE_OVERRIDE)
 
-void *_Nullable malloc(int64_t nb_bytes)
+void *_Nullable malloc(size_t nb_bytes)
 {
-    DEBUG_PRINTF(stderr, "malloc( %ld )\n", nb_bytes);
-
     if (!FreedomPool::real_malloc)
         FreedomPool::initialize_overrides();
+    
+   // DEBUG_PRINTF(stderr, "malloc( %ld )\n", nb_bytes);
+    total_alloc += nb_bytes;
+ 
+    if (total_alloc > total_max_alloc) {
+        if (nb_bytes >= THRESH_DEBUG_PRINT)
+            DEBUG_PRINTF(stderr, "malloc( %2ld MB ) max: %3lld MB\n", nb_bytes/MBYTE, total_max_alloc/MBYTE);
+        total_max_alloc = total_alloc;
+    }
     
     return bigpool.malloc(nb_bytes);
 }
 
-void free(void *_Nonnull p) {
-    DEBUG_PRINTF(stderr, "free( %ld )\n", (long)p);
+void free(void *_Nullable p)
+{
+    int64_t space = 0;
 
     if (!FreedomPool::real_free)
         FreedomPool::initialize_overrides();
     
-   // DEBUG_PRINTF(stderr, "free( %ld )\n", nb_bytes);
-    bigpool.free(p);
+    // DEBUG_PRINTF(stderr, "free( %ld )\n", nb_bytes);
+
+    if (p) { space = bigpool.malloc_size(p); bigpool.free(p); }
+    if (space > 0) {
+        total_alloc -= space;
+    }
 }
 
 /*
-size_t malloc_usable_size(const void *_Nonnull ptr)
+size_t malloc_usable_size(const void *_Nullable ptr)
 {
     DEBUG_PRINTF(stderr, "malloc_usable_size( %ld )\n", (long)ptr);
 
@@ -56,73 +82,135 @@ size_t malloc_usable_size(const void *_Nonnull ptr)
 }*/
 
 
-size_t malloc_size(const void *_Nonnull ptr)
+size_t malloc_size(const void *_Nullable ptr)
 {
-    DEBUG_PRINTF(stderr, "malloc_size( %ld )\n", (long)ptr);
-
     if (!FreedomPool::real_malloc_size)
         FreedomPool::initialize_overrides();
     
-    return bigpool.malloc_size(ptr); 
+   //  DEBUG_PRINTF(stderr, "malloc_size( %ld )\n", (long)ptr);
+
+    return bigpool.malloc_size(ptr);
 }
 
-void *_Nullable realloc(void *_Nonnull p, size_t new_size)
+void *_Nullable realloc(void *_Nullable p, size_t new_size)
 {
-    DEBUG_PRINTF(stderr, "realloc( %ld, %ld )\n", (long)p, new_size);
+    void *_Nullable ret = NULL;
+    
     if (!FreedomPool::real_realloc)
         FreedomPool::initialize_overrides();
-    return bigpool.realloc(p, new_size);
+    
+    // DEBUG_PRINTF(stderr, "realloc( %ld, %ld )\n", (long)p, new_size);
+    int64_t space = 0;
+    if (p) { space = bigpool.malloc_size(p); }
+   
+    ret = bigpool.realloc(p, new_size);
+    
+    // don't count freedompool extend
+    if (ret) {
+        total_alloc += new_size - space;
+    }
+    if (total_alloc > total_max_alloc)
+    {
+        if (new_size >= THRESH_DEBUG_PRINT)
+            DEBUG_PRINTF(stderr, "   new( %2ld MB ) max: %3lld MB\n", (long)(new_size/MBYTE), (long)(total_max_alloc/MBYTE));
+#ifdef BREAK_ON_THRESH
+        if (new_size >= THRESH_DEBUG_BREAK)
+            DEBUGGER_BREAK
+#endif
+            total_max_alloc = total_alloc;
+    }
+    return ret;
 }
 
 void *_Nullable calloc(size_t count, size_t size)
 {
-    DEBUG_PRINTF(stderr, "calloc( %ld, %ld )\n", (long)count, size);
     if (!FreedomPool::real_calloc)
         FreedomPool::initialize_overrides();
+    
+    //  DEBUG_PRINTF(stderr, "calloc( %ld, %ld )\n", (long)count, size);
+    size_t tot = size * count;
+
+    total_alloc += tot;
+    if (total_alloc > total_max_alloc) 
+    {
+        if (tot >= THRESH_DEBUG_PRINT)
+            DEBUG_PRINTF(stderr, "calloc( %2ld MB ) max: %3lld MB\n", (long)(tot/MBYTE), (long)(total_max_alloc/MBYTE));
+#ifdef BREAK_ON_THRESH
+        if (tot >= THRESH_DEBUG_BREAK)
+            DEBUGGER_BREAK
+#endif
+        total_max_alloc = total_alloc;
+    }
+    
     return bigpool.calloc(count, size);
 }
 #endif
 
 #if !defined(DISABLE_NEWDELETE_OVERRIDE)
-void * operator new(std::size_t n) {
-    total_alloc += n;
-    if (n > 1024L*1024L*5L) {
-        //Debugger();
-        DEBUG_PRINTF(stderr, "new( %ld MB ) tot: %ld MB\n", n/1024L/1024L, total_alloc/1024L/1024L);
-    }
+
+void * operator new(std::size_t n) 
+{
     if (!FreedomPool::real_malloc)
         FreedomPool::initialize_overrides();
+    
+    total_alloc += n;
+    if (total_alloc > total_max_alloc) {
+        if (n >= THRESH_DEBUG_PRINT)
+            DEBUG_PRINTF(stderr, "   new( %2ld MB ) max: %3lld MB\n", (long)(n/MBYTE), (size_t)(total_max_alloc/MBYTE));
+#ifdef BREAK_ON_THRESH
+        if (n >= THRESH_DEBUG_BREAK)
+            DEBUGGER_BREAK
+#endif
+            total_max_alloc = total_alloc;
+    }
+    
     return bigpool.malloc(n);
 }
 
-void operator delete(void * p) throw() {
-    size_t space = 0;
-    DEBUG_PRINTF(stderr, "delete ( %ld )\n", (long)p);
+void operator delete(void *_Nullable p) throw()
+{
     if (!FreedomPool::real_free)
         FreedomPool::initialize_overrides();
-    if (p) { space = bigpool.malloc_size(p); bigpool.free(p); }
+
+    int64_t space = 0;
+    if (p) { space = bigpool.malloc_size(p); }
+    //DEBUG_PRINTF(stderr, "delete ( %x )\n", p);
     if (space > 0)
         total_alloc -= space;
+    bigpool.free(p);
 }
 
-void *operator new[](std::size_t n) {
-    total_alloc += n;
-    if (n > 1024L*1024L*5L) {
-       // Debugger();
-        DEBUG_PRINTF(stderr, "new[] ( %ld MB ) tot: %ld MB\n", (long)n/1024L/1024L, total_alloc/1024L/1024L);
-    }
+void *operator new[](std::size_t n) 
+{
     if (!FreedomPool::real_malloc)
         FreedomPool::initialize_overrides();
+    
+    total_alloc += n;
+    if (total_alloc > total_max_alloc) {
+       if (n >= THRESH_DEBUG_PRINT)
+            DEBUG_PRINTF(stderr, " new[]( %2ld MB ) max: %3lld MB\n", (long)(n/MBYTE), (long)(total_max_alloc/MBYTE));
+#ifdef BREAK_ON_THRESH
+        if (n >= THRESH_DEBUG_BREAK)
+            DEBUGGER_BREAK
+#endif
+        total_max_alloc = total_alloc;
+    }
+    
     return bigpool.malloc(n);
 }
-void operator delete[](void *p) throw() {
-    size_t space = 0;
-    DEBUG_PRINTF(stderr, "delete[] ( %ld )\n", (long)p);
+
+void operator delete[](void *p) throw()
+{
     if (!FreedomPool::real_free)
         FreedomPool::initialize_overrides();
-    if (p) {  space = bigpool.malloc_size(p); bigpool.free(p); }
+
+    int64_t space = 0;
+    if (p) { space = bigpool.malloc_size(p); }
+  //  DEBUG_PRINTF(stderr, "delete[] ( %x )\n", p);
+   
     if (space > 0)
         total_alloc -= space;
+    bigpool.free(p);
 }
 
 #endif
