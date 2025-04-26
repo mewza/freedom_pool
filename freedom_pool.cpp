0d12a7d1107470c0ea3d8ae37ba779be6084843a
mewza <subband@gmail.com>
2025-04-26 13:41:52 -1000

    Update freedom_pool.cpp

--- freedom_pool.cpp
+++ freedom_pool.cpp
@@ -1,11 +1,11 @@
-//  freedom_pool.h v1.4 (c)2023-2025 Dmitry Boldyrev
+//  freedom_pool.cpp v1.5 (c)2023-2025 Dmitry Boldyrev
 //
 //  This is the most efficient block-pool memory management system you can find.
 //  I tried many before writing my own: rpmalloc, tlsf, etc.
 //  This code is partially based off this block allocator concept:
 //  https://www.codeproject.com/Articles/1180070/Simple-Variable-Size-Memory-Block-Allocator
 //
-//  NEW (v1.4): Optimizatoins and configurable memory alignment. Got rid of multimap bottleneck.
+//  NEW (v1.5): Implemented IsValidPointer for safety, modernized volatile m_Internal with std::atomic
 
 #include "freedom_pool.h"
 
@@ -16,19 +16,18 @@
 #define BIGPOOL_CALLOC bigpool.calloc
 #define BIGPOOL_REALLOC bigpool.realloc
 
-#ifdef __cplusplus
+#define ABS(x) (((x)<0)?-(x):(x))
+#define PRINT_V(x) ((ABS(x)/MBYTE) > 0) ? (x)/MBYTE : (x)/KBYTE, (((x)/MBYTE) > 0) ? "MB" : "kb"
 
-#define PRINT_V(x) (((x)/MBYTE) > 0) ? (x)/MBYTE : (x)/KBYTE, (((x)/MBYTE) > 0) ? "MB" : "kb"
+real_malloc_ptr _Nullable real_malloc = nullptr;
+real_free_ptr _Nullable real_free = nullptr;
+real_calloc_ptr _Nullable real_calloc = nullptr;
+real_realloc_ptr _Nullable real_realloc = nullptr;
+real_malloc_size_ptr _Nullable real_malloc_size = nullptr;
+real_malloc_usable_size_ptr _Nullable real_malloc_usable_size = nullptr;
 
-real_malloc_ptr _Nullable real_malloc = NULL;
-real_free_ptr _Nullable real_free = NULL;
-real_calloc_ptr _Nullable real_calloc = NULL;
-real_realloc_ptr _Nullable real_realloc = NULL;
-real_malloc_size_ptr _Nullable real_malloc_size = NULL;
-real_malloc_usable_size_ptr _Nullable real_malloc_usable_size = NULL;
-
-static size_t heap_alloc = 0L;
-static size_t heap_max_alloc = 0L;
+static int64_t heap_alloc = 0L;
+static int64_t heap_max_alloc = 0L;
 
 void reset_freedom_counters(void)
 {
@@ -44,10 +43,10 @@ void *_Nullable malloc(size_t nb_bytes)
 {
 #ifdef FREEDOM_DEBUG
     heap_alloc += nb_bytes;
-    heap_max_alloc = F_MAX(heap_max_alloc, (size_t)nb_bytes);
+    heap_max_alloc = std::max(heap_max_alloc, (int64_t)nb_bytes);
     
     if (nb_bytes >= THRESH_DEBUG_PRINT)
-        DEBUG_PRINTF(stderr, "malloc( %3ld %s ) heap: %3ld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "malloc( %8ld %s ) heap: %3lld %s max: %3lld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc), PRINT_V(heap_max_alloc));
 #endif
     return BIGPOOL_MALLOC(nb_bytes);
 }
@@ -55,13 +54,13 @@ void *_Nullable malloc(size_t nb_bytes)
 void free(void *_Nullable ptr)
 {
 #ifdef FREEDOM_DEBUG
-    size_t space = 0;
+    int64_t space = 0;
     if (ptr) { space = BIGPOOL_SIZE(ptr); }
     if (space > 0) {
         heap_alloc -= space;
     }
     if (space >= THRESH_DEBUG_PRINT)
-        DEBUG_PRINTF(stderr, "  free( %3ld %s ) heap: %3ld %s\n", PRINT_V(space), PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "  free( %3lld %s ) heap: %3lld %s\n", PRINT_V(space), PRINT_V(heap_alloc));
 #endif
     BIGPOOL_FREE(ptr);
 }
@@ -87,7 +86,7 @@ void *_Nullable realloc(void *_Nullable ptr, size_t nb_bytes)
     size_t space = 0;
     if (ptr) { space = BIGPOOL_SIZE(ptr); }
     
-    void *_Nullable ret = NULL;
+    void *_Nullable ret = nullptr;
     ret = BIGPOOL_REALLOC(ptr, nb_bytes);
     
     // don't count freedompool extend
@@ -100,7 +99,7 @@ void *_Nullable realloc(void *_Nullable ptr, size_t nb_bytes)
 #endif
     heap_max_alloc = std::max(heap_max_alloc, heap_alloc);
     if (nb_bytes >= THRESH_DEBUG_PRINT)
-        DEBUG_PRINTF(stderr, "realloc( %3ld %s ) heap: %3ld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "realloc( %8ld %s ) heap: %3lld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc));
     return ret;
 #endif
     return BIGPOOL_REALLOC(ptr, nb_bytes);
@@ -118,7 +117,7 @@ void *_Nullable calloc(size_t count, size_t size)
         DEBUGGER
 #endif
     if (nb_bytes >= THRESH_DEBUG_PRINT)
-        DEBUG_PRINTF(stderr, "calloc( %3ld %s ) heap: %3ld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "calloc( %8ld %s ) heap: %3lld %s %3lld %s\n", PRINT_V(nb_bytes), PRINT_V(heap_alloc), PRINT_V(heap_max_alloc));
 #endif
     return BIGPOOL_CALLOC(count, size);
 }
@@ -137,7 +136,7 @@ void * operator new(std::size_t nb_bytes)
 #endif
     heap_max_alloc = std::max(heap_max_alloc, heap_alloc);
     if (nb_bytes >= THRESH_DEBUG_PRINT)
-        DEBUG_PRINTF(stderr, "   new( %3lld %s %7x ) heap: %3ld %s\n", PRINT_V(nb_bytes), ptr,  PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "   new( %3lld %s %7x ) heap: %3lld %s max: %3lld %s\n", PRINT_V(nb_bytes), ptr,  PRINT_V(heap_alloc), PRINT_V(heap_max_alloc));
 #endif
     
     return ptr;
@@ -151,7 +150,7 @@ void operator delete(void *_Nullable ptr) throw()
     if (space > 0)
         heap_alloc -= space;
     if (space >= THRESH_DEBUG_BREAK)
-        DEBUG_PRINTF(stderr, "delete( %3lld %s %7x) heap: %3ld %s\n", PRINT_V(space), ptr, PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, "delete( %3lld %s %7x) heap: %3lld %s\n", PRINT_V(space), ptr, PRINT_V(heap_alloc));
 #endif
      BIGPOOL_FREE(ptr);
 }
@@ -163,7 +162,7 @@ void *operator new[](std::size_t nb_bytes)
     heap_alloc += nb_bytes;
     heap_max_alloc = std::max(heap_max_alloc, heap_alloc);
     if (nb_bytes >= THRESH_DEBUG_PRINT)
-            DEBUG_PRINTF(stderr, " new[]( %3ld %s %7x ) heap: %3ld %s\n", PRINT_V(nb_bytes), ptr,  PRINT_V(heap_alloc));
+            DEBUG_PRINTF(stderr, " new[]( %8ld %s %7x ) heap: %3lld %s %3lld %s\n", PRINT_V(nb_bytes), ptr,  PRINT_V(heap_alloc), PRINT_V(heap_max_alloc));
 #ifdef BREAK_ON_THRESH
     if (nb_bytes >= THRESH_DEBUG_BREAK)
         DEBUGGER
@@ -181,11 +180,9 @@ void operator delete[](void *ptr) throw()
     if (space > 0)
         heap_alloc -= space;
     if (space >= THRESH_DEBUG_BREAK)
-        DEBUG_PRINTF(stderr, " del[]( %3ld %s %7x ) heap: %3ld %s\n", PRINT_V(space), ptr, PRINT_V(heap_alloc));
+        DEBUG_PRINTF(stderr, " del[]( %8ld %s %7x ) heap: %3lld %s\n", PRINT_V(space), ptr, PRINT_V(heap_alloc));
 #endif
     BIGPOOL_FREE(ptr);
 }
 
 #endif // DISABLE_NEWDELETE_OVERRIDE
-
-#endif // __cplusplus
